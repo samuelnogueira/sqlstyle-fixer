e483e58abf283ec19f91ec840543a6bb2d7ed0c6
Samuel Nogueira <smcientistamaluco@gmail.com>
2024-01-13 23:46:46 +0000

    Add another bad example

---
+++ tests/bad-examples/example_4.fixed.sql
@@ -0,0 +1,11 @@
+SELECT column_name1,
+       column_name2,
+       column_name3
+  FROM table_1
+  JOIN table_2
+    ON table_1.id = table_2.id
+ WHERE clouds = true
+   AND gem = true
+ GROUP BY 1,2,3
+HAVING column_name1 > 0
+   AND column_name2 > 0
