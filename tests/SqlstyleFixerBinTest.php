bfeeaf5b9bcb65aedff26fc4e8812c4cd6eb6621
Samuel Nogueira <smcientistamaluco@gmail.com>
2024-05-01 01:09:41 +0100

    Add bin script to be used as an external formatter

---
+++ tests/SqlstyleFixerBinTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Samuelnogueira\SqlstyleFixerTests;
+
+use PHPUnit\Framework\TestCase;
+
+use Symfony\Component\Process\PhpSubprocess;
+
+final class SqlstyleFixerBinTest extends TestCase
+{
+    public function testStdinStdout(): void
+    {
+        $process = new PhpSubprocess([__DIR__ . '/../bin/sqlstyle-fixer']);
+        $process
+            ->setInput(
+                <<<'SQL'
+select file_hash  -- stored ssdeep hash
+from file_system where file_name = '.vimrc';
+SQL,
+            )
+            ->mustRun();
+
+        self::assertEquals(
+            <<<'SQL'
+SELECT file_hash  -- stored ssdeep hash
+  FROM file_system
+ WHERE file_name = '.vimrc';
+SQL,
+            $process->getOutput(),
+        );
+    }
+}
